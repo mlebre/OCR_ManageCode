9f67841a0b0ea4ce3f1ef9929680246402e3aae9
Marianne Lebre <lebre@genoway.com>
2019-05-24 10:00:46 +0200

    Return error is one of the argument is not a number

--- calculator.pl
+++ calculator.pl
@@ -2,11 +2,14 @@
 
 use strict;
 use warnings;
+use Scalar::Util qw(looks_like_number);
 
 my $a = $ARGV[0];
 my $b = $ARGV[1];
 if(! defined $a or ! defined $b){
 	die("This calculator program requires 2 numbers after launching it, please relaunch it with them.\n");
+} elsif(! looks_like_number($a) or ! looks_like_number($b)){
+	die("Please provide 2 numbers\n");
 }
 print "$a + $b = ".($a+$b)."\n";
 print "$a - $b = ".($a-$b)."\n";
