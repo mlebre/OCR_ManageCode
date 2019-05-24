942b5a183a7bb022ed017679dd63eac4bc2f6ff2
Marianne Lebre <lebre@genoway.com>
2019-05-24 09:35:53 +0200

    Add error message if 2 numbers are not provided

--- calculator.pl
+++ calculator.pl
@@ -5,7 +5,9 @@ use warnings;
 
 my $a = $ARGV[0];
 my $b = $ARGV[1];
-
+if(! defined $a or ! defined $b){
+	die("This calculator program requires 2 numbers after launching it, please relaunch it with them.\n");
+}
 print "$a + $b = ".($a+$b)."\n";
 print "$a - $b = ".($a-$b)."\n";
 print "$a * $b = ".($a*$b)."\n";
