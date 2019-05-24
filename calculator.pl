d1c953d6de01db1a361242ef4329d0f47e3131a7
Marianne Lebre <lebre@genoway.com>
2019-05-24 09:34:28 +0200

    Perform 4 basical operations on 2 given numbers

---
+++ calculator.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $a = $ARGV[0];
+my $b = $ARGV[1];
+
+print "$a + $b = ".($a+$b)."\n";
+print "$a - $b = ".($a-$b)."\n";
+print "$a * $b = ".($a*$b)."\n";
+print "$a / $b = ".($a/$b)."\n";
